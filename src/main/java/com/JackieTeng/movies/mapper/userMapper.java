572e75fd80924e277525d670552d05266d86a873
JJJackieDeng <15975403320@163.com>
2020-02-06 23:18:11 +0800

    第一次提交#1

---
+++ src/main/java/com/JackieTeng/movies/mapper/userMapper.java
@@ -0,0 +1,16 @@
+package com.JackieTeng.movies.mapper;
+
+import com.JackieTeng.movies.pojo.User;
+
+import java.util.List;
+
+/**
+ * @Author jackie
+ * @Email 15975403320@163.com
+ * @Date 2020/2/6 23:10
+ * @Description
+ */
+public interface userMapper {
+    /*查询所有*/
+    public List<User> findall();
+}
