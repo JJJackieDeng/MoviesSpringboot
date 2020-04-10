29e43c4efd88c77bb550fb6ea47f27c985cdb04d
JJJackieDeng <15975403320@163.com>
2020-04-10 15:58:06 +0800

    跨域配置完成

--- src/main/java/com/jackiedeng/movies/config/CORSConfiguration.java
+++ src/main/java/com/jackiedeng/movies/config/CORSConfiguration.java
@@ -1,14 +1,8 @@
 package com.jackiedeng.movies.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-import java.rmi.registry.Registry;
-
-import static sun.tools.jconsole.Messages.ALL;
 
 /**
  * @Author jackie
@@ -31,5 +25,4 @@ public class CORSConfiguration implements WebMvcConfigurer {
                      .maxAge(36000)
                      .allowCredentials(true);
          }
-
 }
